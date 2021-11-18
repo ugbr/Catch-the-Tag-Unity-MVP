a6b9aa8fd90d4f884d82bcfe7853b69df2621d05
Uguudei Bayaraa <uguudei.bayar@gmail.com>
2021-11-17 19:20:02 -0600

    first commit

---
+++ Catch the Tags/Assets/Scripts/HatMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatMovement : MonoBehaviour
+{
+
+    private Rigidbody2D myBody;
+
+    public float speed, xBound; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0)
+        {
+          myBody.velocity = Vector2.right * speed;
+        } else if (h < 0)
+        {
+          myBody.velocity = Vector2.left * speed;
+        } else
+        {
+          myBody.velocity = Vector2.zero;
+        }
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -xBound, xBound),
+          transform.position.y);
+        print(h);
+    }
+}
